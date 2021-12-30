f75fbe1592111ad34a7d7ca38e36e0b2558e0f2f
tuhin-thinks <tuhinmitra190221@gmail.com>
2021-12-31 04:14:03 +0530

    major update in generating image from any number of images and optimizing final image (on demand)

--- Image-Collage/README.md
+++ Image-Collage/README.md
@@ -1,31 +1,42 @@
 # Create Image Collage
 **Pre-requisites**
-    
+
     python >=3.7
 
 Other modules required:
-    
+
 ```bash
 pip install pillow
+pip install tqdm
 ```
 
 To launch the application:
 ```python
 python create_collage.py
 ```
+To run application for optimizing images:
+```python
+python create_collage.py optimize
+```
+**and follow on screen input instructions to optimized image*
 <hr>
 
-**Steps to operate:**
-
-1. Enter directory to scan for images.
-2. Enter image formats to consider for making the collage (png, jpg, jpeg, webp)
-3. Enter
-
->> A file named "collage.png" will be generated in the present working directory.
+**A file named "collage..n.png" will be generated in the present working directory.**
+* where n is the number of files **already existing** in target output directory.
 <hr>
 
-## Sample output
+## Sample output(s)
 
 <p align="center">
-    <img src="collage.png" width="450">
+    <img src=".images/collage.png" width="500">
+    <br>
+    <u>Sample barcodes collaged together</u>
+</p>
+
+<br>
+
+<p align="center">
+    <img src=".images/collage6_resized.png" width="500">
+    <br>
+    <u><b>2979</b> images collaged together</u>
 </p>
--- Image-Collage/create_collage.py
+++ Image-Collage/create_collage.py
@@ -1,83 +1,205 @@
 import os
+import re
+import statistics as stats
+import sys
+import typing
+from functools import lru_cache
 
-from PIL import Image
+from PIL import Image  # requires installation !pip install pillow
+from tqdm import tqdm
+
+Image.MAX_IMAGE_PIXELS = (
+    None  # setting this value to compress images having size of 600MB+
+)
+
+
+def optimize_image(inp_path, size_tuple, op_path):
+    if os.path.exists(inp_path):
+        print("\n[+] Loading image...")
+        image_ = Image.open(inp_path)
+        print("[+] image loaded\n")
+
+        print("[+] Resizing image...")
+        image_ = image_.resize(size_tuple, Image.ANTIALIAS)
+        print("[+] image resized\n")
+
+        print("[+] Saving image...")
+        image_.save(op_path, optimize=True, quality=90)
+        print("Image saved successfully\n")
 
 
 def get_height_for_w(size_tuple: tuple, width: int):
-    width, height = size_tuple
-    aspect_ratio = width/height
+    width_img, height_img = size_tuple
+    aspect_ratio = width_img / height_img
     height = width / aspect_ratio
     return height
 
-def get_image(width, location):
-    image = Image.open(location).convert('RGBA')
+
+def get_width_for_height(size_tuple: tuple, height: int):
+    width_img, height_img = size_tuple
+    aspect_ratio = width_img / height_img
+    width = height * aspect_ratio
+    return width
+
+
+def get_image(required_width, required_height, location):
+    """Utiliyt function to get the resized image that exactly fits inside the box given
+
+    Args:
+        required_width: width of each image frame inside collage
+        required_height: height of the image frame inside collage
+    """
+
+    image = Image.open(location).convert("RGBA")
     size_tuple = image.size
-    height = get_height_for_w(size_tuple, width)
-    image = image.resize((width, int(height)))
+    height = get_height_for_w(size_tuple, required_width)
+    required_height_ = required_height or int(height)
+    image = image.resize((required_width, required_height_))
     return image
 
-def create(directory, image_type):
+
+def get_n_files(target_dir):
+    """Get number of files in the target directory
+
+    Args:
+        :param target_dir: directory to scan for number of files
+    """
+    return len(
+        [
+            x
+            for x in os.listdir(target_dir)
+            if os.path.isfile(os.path.join(target_dir, x))
+        ]
+    )
+
+
+def create(directory, image_type: typing.Union[str, typing.List]):
+    """Primary working function for creating collage and calculations"""
+
     x_pos = 0
     y_pos = 0
     frame_w = 500
     border_w = 2
-    border_h = 3
+    border_h = 2
     image_paths = []
-    
+
     # load required images from specified directory
     for file in os.listdir(directory):
-        if file.endswith(f".{image_type}"):
+        if isinstance(image_type, str) and file.endswith(f".{image_type}"):
+            image_paths.append(os.path.join(directory, file))
+        elif isinstance(image_type, typing.List) and any(
+            (file.lower().endswith(x) for x in image_type)
+        ):
             image_paths.append(os.path.join(directory, file))
 
     # calculate frame heght
-    frame_h = get_image(frame_w, image_paths[0]).height
+    # ------------- calculate max frame height --------
+    # gen_frame_heights = [
+    #     get_image(frame_w, None, img_path).height for img_path in image_paths
+    # ]
+    # mode_frame_h = int(
+    #     max(gen_frame_heights)
+    # )  # modify and assign this value to mode_frame_h, if you want to use some other box height for each image
+    frame_h = 500  # frame height for each image
 
-    # number of images to show per row
-    num_horz_images = 4
-    
-    # calculate final image width and height
+    # ------------> number of images to show per row <----------------
+    num_horz_images = (
+        int(
+            input(
+                f"\n{len(image_paths)} images to be used in collage,\n"
+                f"Enter number of images per row of the collage: "
+            )
+        )
+        or 5
+    )  # if user enters 0, program will paste 5 images per row.
+
+    # ------------> calculate collage image width and height <---------------
     img_w = (frame_w + border_w) * num_horz_images
     img_h = (frame_h + border_h) * (len(image_paths) // num_horz_images)
-    
-    print("Each frame size:", (frame_w, frame_h))
-    print("Collage size:", (img_w, img_h))
-    print(f"No. of Images: {len(image_paths)}")
-    
-    collage = Image.new("RGBA", (img_w,img_h), color=(255,255,255,255))
+
+    # ------------------ print collage image informations ----------------
+    print(
+        f"""
+Each frame size: {(frame_w, frame_h)}
+Collage size:    {(img_w, img_h)}
+No. of Images:   {len(image_paths)}
+"""
+    )
+
+    # ------------ create empty collage image ---------------
+    collage = Image.new("RGBA", (img_w, img_h), color=(255, 255, 255, 255))
 
     # resize and paste images into main image (collage)
-    for file in image_paths:
-        if file.endswith(image_type):
-            image = get_image(frame_w, os.path.join(directory, file))  # returns the resized PIL Image
-            print((x_pos, y_pos), end=" | ")
-            collage.paste(image, (x_pos, y_pos))
-            if (x_pos + frame_w + border_w) < img_w:
-                x_pos += frame_w + border_w
-            else:
-                x_pos = 0
-                y_pos += frame_h + border_h
-                print("\n", "---"*10)
-    
+    for file in tqdm(image_paths, desc="Progress: "):
+        image = get_image(frame_w, None, file)  # returns the resized PIL Image
+
+        # print(
+        #     f"({x_pos}:{image.width},{y_pos}:{image.height})", end=" | "
+        # )  # this print the top-left position of each child image inside collage image.
+
+        collage.paste(image, (x_pos, y_pos))
+        if (x_pos + frame_w + border_w) < img_w:
+            x_pos += frame_w + border_w
+        else:
+            x_pos = 0
+            y_pos += frame_h + border_h
+            # print("\n", "---" * 2 * num_horz_images)
+
     # save image (additionaly user can also enter output path using input prompt)
-    collage.save("collage.png", bitmap_format="png")
-    print("\ncollage saved\n")
+    collage_file_name = f"collage{get_n_files(os.getcwd())}.png"
+    collage.save(collage_file_name, bitmap_format="png")
+    print(f"\ncollage saved as {os.path.realpath(collage_file_name)}\n")
+
 
 def get_path():
-    input_path = input("Enter path to locate images:")
+    """Get path to scan images from."""
+
+    input_path = input(
+        "Enter path to locate images [suffix :relpath for entering relative paths] :"
+    )
+    if input_path.endswith(":relpath"):
+        match_pattern = r"(.*)\:relpath$"
+        input_path = os.path.realpath(re.search(match_pattern, input_path).groups()[0])
+        print(f"  Generated realpath: {input_path}")
     if os.path.exists(input_path):
         return input_path
     else:
         return get_path()
 
+
 def get_image_type():
+    """Get image type to consider for the collage image."""
+
     types = ("png", "jpeg", "jpg", "webp")
-    image_type = input("Enter image type to consider: ")
+    image_type = input(
+        "Enter image type to consider: [Enter ; separated values for mutiple types]: "
+    )
     if image_type in types:
         return image_type
+    elif ";" in image_type:
+        return image_type.strip().split(";")
     else:
         return get_image_type()
 
+
 if __name__ == "__main__":
-    img_path = get_path()
-    img_type = get_image_type()
-    create(img_path, img_type)
+    argv = sys.argv
+    mode = "create"
+    if len(argv) == 2:
+        mode = argv[1]
+    if mode == "create":
+        img_path = get_path()
+        img_type = get_image_type()
+        create(img_path, img_type)
+    elif mode == "optimize":
+        img_to_optimize = input("Enter image path to optimize: ")
+        save_path = input("Enter image save path: ")
+        size_tuple = tuple(
+            map(
+                int,
+                input("Enter size tuple to resize image (space separated): ").split(
+                    " "
+                ),
+            )
+        )
+        optimize_image(img_to_optimize, size_tuple, save_path)
